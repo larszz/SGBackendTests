f2f3918048b593483049bd938e6a55b0a7595d1d
LarsZi <la.zi@t-online.de>
2021-04-19 12:02:54 +0200

    CC (Renaming of variables, String output)

--- main.py
+++ main.py
@@ -1,44 +1,49 @@
 import random
 import time
 
-testpath = "/home/work/SGTest/PythonTest/"
-filename = "teststrings_100.txt"
+directory = "/home/work/SGTest/PythonTest/"
+samples_filename = "teststrings_100.txt"
 
-testfiles_prefix = "testfile"
+file_prefix = "testfile"
 
 
 # number of files to create
-numoffiles = 3
+number_of_files = 3
 
 # number of changes
-numofchanges = 50
-if numofchanges > 100:
-    filename = "teststrings_1000.txt"
-elif numofchanges > 1000:
-    filename = "teststrings_100000.txt"
+number_of_changes = 10
+
+# set file for sample data depending on the needed number of changes
+if number_of_changes > 100:
+    samples_filename = "teststrings_1000.txt"
+elif number_of_changes > 1000:
+    samples_filename = "teststrings_100000.txt"
+
+sleeptime = 1  # type: float
 
 if __name__ == '__main__':
-    print(str(numofchanges) + " Changes -> " + "Using Data from " + str(filename))
+    print("Using Testdata from " + str(samples_filename))
+    print("Sleep interval: " + str(sleeptime) + "s\n\n")
 
-    with open(filename) as f:
+    with open(samples_filename) as file:
 
-        content = f.readlines()
+        sample_lines = file.readlines()
 
         filelist = []
-        for i in range(numoffiles):
-            fn = testpath + testfiles_prefix + str(i) + ".txt"
+        for i in range(number_of_files):
+            fn = directory + file_prefix + str(i) + ".txt"
             filelist.append(fn)
 
 
         # make file changes
         fileindex = 0
-        for i in range(numofchanges):
+        for i in range(number_of_changes):
             print(str(i) + "...")
-            f = open(filelist[fileindex], "w")
-            f.write(content[i])
-            f.close()
-            fileindex = (fileindex + 1) % numoffiles
-            time.sleep(0.5)
+            file = open(filelist[fileindex], "w")
+            file.write(sample_lines[i])
+            file.close()
+            fileindex = (fileindex + 1) % number_of_files
+            time.sleep(sleeptime)
 
 
 
